4aadcb938b19926afb720bd2c5bdd9ee9d912584
Esteban <estebanavella1@gmail.com>
2022-10-31 23:25:02 -0300

    final

--- backendAvellaspring/src/main/java/com/backend/backendAvella/Controller/PersonaController.java
+++ backendAvellaspring/src/main/java/com/backend/backendAvella/Controller/PersonaController.java
@@ -15,12 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin( origins = "http://localhost:4200/" )
+@CrossOrigin( origins = "https://argentinaprogramaavellaesteban.web.app" )
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
